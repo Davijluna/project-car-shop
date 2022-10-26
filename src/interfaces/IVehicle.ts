1c2402c626f136f67d3c71a1497f47eb6d1679bf
davi <davijluna@gmail.com>
2022-10-25 23:00:45 -0300

    refactor: adicionando message de erros do valores min de interface IVehicle

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -2,14 +2,14 @@ import { z } from 'zod';
 
 const vehicleZodSchema = z.object({
   model: z.string()
-    .min(3),
+    .min(3, { message: 'Must be 5 or more characters long' }),
   year: z.number()
     .int()
     .positive()
     .gte(1900)
     .lte(2022),
   color: z.string()
-    .min(3),
+    .min(3, { message: 'Must be 5 or more characters long' }),
   status: z.boolean().optional(),
   buyValue: z.number().int(),
 });
