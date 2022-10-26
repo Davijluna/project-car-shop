431fdcaeaa359c4de023a3953396ccd03cae2c9d
davi <davijluna@gmail.com>
2022-10-26 00:14:31 -0300

    feat: criando Schema de Car em src

---
+++ src/models/Car.ts
@@ -0,0 +1,20 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { ICar } from '../interfaces/ICar';
+import MondoModel from './MongoModel';
+
+const carMongooseSchema = new Schema<ICar>({
+  status: Boolean,
+  model: String,
+  year: Number,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+}, { versionKey: false });
+
+class Car extends MondoModel<ICar> {
+  constructor(model = mongooseCreateModel('Car', carMongooseSchema)) {
+    super(model);
+  }
+}
+
+export default Car;
