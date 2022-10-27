a4c13495256fc99365cfa447c6338df61b5e5f79
davi <davijluna@gmail.com>
2022-10-27 11:31:12 -0300

    feat: requisito 04

--- src/tests/unit/services/seu-teste-service.test.ts
+++ src/tests/unit/services/seu-teste-service.test.ts
@@ -1,8 +1,8 @@
-// template para criação dos testes de cobertura da camada de service
+// // template para criação dos testes de cobertura da camada de service
 
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
+// import * as sinon from 'sinon'; //
+// import chai from 'chai'; //
 // const { expect } = chai;
 
 // describe('Sua descrição', () => {
